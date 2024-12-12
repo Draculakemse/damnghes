37233520712a1b483dd4e5fcc7bd6e5caa2f263d
Draculakemse <welcomechen22@gmail.com>
2024-12-12 21:49:29 +0000

    Update 3.lua

--- 3.lua
+++ 3.lua
@@ -24,11 +24,10 @@ end
 -- Get and process currency amounts
 local rawBucksAmount = game:GetService("Players").LocalPlayer.PlayerGui.BucksIndicatorApp.CurrencyIndicator.Container.Amount.Text
 local bucksAmount = rawBucksAmount:gsub(",", "") -- Remove commas from the raw bucks amount
-local formattedBucksAmount = formatNumber(bucksAmount) -- Format the bucks amount
 
 local rawGingerAmount = game:GetService("Players").LocalPlayer.PlayerGui.AltCurrencyIndicatorApp.CurrencyIndicator.Container.Amount.Text
 local gingerAmount = rawGingerAmount:gsub(",", "") -- Remove commas from the raw ginger amount
-local formattedGingerAmount = formatNumber(gingerAmount) -- Format the gingerbread amount
+local formattedGingerAmount = formatNumber(gingerAmount)
 
 -- Initialize variables
 local Counter = 0
@@ -86,7 +85,7 @@ wait(1)
 
 -- Prepare data for the webhook
 local data = {
-    ["content"] = ("BOSS <@" .. discordid .. "> 🤖 " .. localPlayer.Name .. " has 🎅 [ADOPT ME] 🎅 " .. Counter .. " Age Potion + " .. formattedBucksAmount .. " Bucks + " .. formattedGingerAmount .. " Gingerbread"),
+    ["content"] = ("BOSS <@" .. discordid .. "> 🤖 " .. localPlayer.Name .. " has 🎅 [ADOPT ME] 🎅 " .. Counter .. " Age Potion + " .. bucksAmount .. " Bucks + " .. formattedGingerAmount .. " Gingerbread"),
 }
 local newdata = game:GetService("HttpService"):JSONEncode(data)
 
